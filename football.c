00400f38ea22e65f4dd865fda9b4974c61a5a1e0
N-Heyer <156978640+N-Heyer@users.noreply.github.com>
2025-03-26 17:04:57 -0500

    comments

--- football.c
+++ football.c
@@ -1,18 +1,33 @@
-#include <stdio.h>
+/*
+Author: Nick Heyer
+KUID: 3142337
+Date: 3/26/25
+Lab: Assignment2
+Last modified: 3/26/25
+EECS 348 Lab 7 
+Football score possibilities
+input: user input
+Output: all the possible nfl score combinations based on the user's inputted number
+Collaborators: ChatGPT was used to find syntax errors and help with the structure. 
+*/
 
+#include <stdio.h>
+// func to find all possibile combos of scores for a given num
 void findCombinations(int score) {
     printf("Possible combinations of scoring plays for score %d:\n", score);
 
-    int found = 0;
-    for (int td2pt = 0; td2pt <= score / 8; td2pt++) {
-        for (int td1pt = 0; td1pt <= score / 7; td1pt++) {
-            for (int td = 0; td <= score / 6; td++) {
-                for (int fg = 0; fg <= score / 3; fg++) {
-                    for (int safety = 0; safety <= score / 2; safety++) {
+    int found = 0; // check if one valid combo is found
+    for (int td2pt = 0; td2pt <= score / 8; td2pt++) { // TD w 2PT conv 8PT
+        for (int td1pt = 0; td1pt <= score / 7; td1pt++) { // TD w FG 7 PT
+            for (int td = 0; td <= score / 6; td++) { // touch w extra PT 6 PT
+                for (int fg = 0; fg <= score / 3; fg++) { // FG 3 PT
+                    for (int safety = 0; safety <= score / 2; safety++) { // safety 2 pt
+                         // calc the total score from currenmt combo
                         if (td2pt * 8 + td1pt * 7 + td * 6 + fg * 3 + safety * 2 == score) {
+                            // print valid score combos
                             printf("%d TD + 2pt, %d TD + FG, %d TD, %d FG, %d Safety\n",
                                    td2pt, td1pt, td, fg, safety);
-                            found = 1;
+                            found = 1; // atleast one valid combo exist
                         }
                     }
                 }
@@ -20,26 +35,26 @@ void findCombinations(int score) {
         }
     }
 
-    if (!found) {
-        printf("No valid scoring combos for %d.\n", score);
+    if (!found) { // if no valid combos are found notify the user
+        printf("No valid scoring combos for %d.\n", score); 
     }
-    printf("\n");
+    printf("\n"); 
 }
 
 int main() {
-    int score;
+    int score; // store user input
 
-    while (1) {
+    while (1) { // loop to continuosly prompt the user until termination
         printf("Enter the NFL score (0 or 1 to STOP): ");
-        scanf("%d", &score);
+        scanf("%d", &score); // take user input
 
-        if (score <= 1) {
+        if (score <= 1) { // if score is 0 or 1 exit loop
             break;
         }
 
-        findCombinations(score);
+        findCombinations(score); // call func to compute and print valid score combos
     }
 
-    printf("Program Term.\n");
-    return 0;
+    printf("Program Term.\n"); // proves program termination
+    return 0; // return
 }
--- temp.c
+++ temp.c
@@ -35,3 +35,72 @@ void categorize_temperature(float celsius) {
         printf("Weather advisory: Stay indoors.\n");
     }
 }
+
+int main() {
+    float temperature;
+    int current_scale, target_scale;
+
+    while (1) {
+        printf("Enter the temperature: ");
+        scanf("%f", &temperature);
+
+        printf("Choose the current scale (1) Celsius, (2) Fahrenheit, (3) Kelvin: ");
+        scanf("%d", &current_scale);
+
+        printf("Convert to (1) Celsius, (2) Fahrenheit, (3) Kelvin: ");
+        scanf("%d", &target_scale);
+
+        if (current_scale == target_scale) {
+            printf("Current scale and target scale cannot be the same. Try again.\n");
+            continue;
+        }
+
+        float converted_temperature;
+        float converted_to_celsius;
+
+        if (current_scale == 1) { 
+            converted_to_celsius = temperature;
+            if (target_scale == 2) {
+                converted_temperature = celsius_to_fahrenheit(temperature);
+            } else { 
+                converted_temperature = celsius_to_kelvin(temperature);
+            }
+        } else if (current_scale == 2) { 
+            converted_to_celsius = fahrenheit_to_celsius(temperature);
+            if (target_scale == 1) {
+                converted_temperature = converted_to_celsius; 
+            } else { 
+                converted_temperature = celsius_to_kelvin(converted_to_celsius);
+            }
+        } else { 
+            converted_to_celsius = kelvin_to_celsius(temperature);
+            if (target_scale == 1) {
+                converted_temperature = converted_to_celsius; 
+            } else { 
+                converted_temperature = celsius_to_fahrenheit(converted_to_celsius);
+            }
+        }
+
+        printf("Converted temperature: %.2f", converted_temperature);
+        if (target_scale == 1) {
+            printf("°C\n");
+        } else if (target_scale == 2) {
+            printf("°F\n");
+        } else {
+            printf("K\n");
+        }
+
+
+        categorize_temperature(converted_to_celsius);
+
+
+        char repeat;
+        printf("Do you want to perform another conversion? (y/n): ");
+        scanf(" %c", &repeat);
+        if (repeat != 'y' && repeat != 'Y') {
+            break;
+        }
+    }
+
+    return 0;
+}
