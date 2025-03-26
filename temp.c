d55919340614c2cbc81d594150bfc5c067748b81
N-Heyer <156978640+N-Heyer@users.noreply.github.com>
2025-03-26 16:51:35 -0500

    advisory based on cels

--- temp.c
+++ temp.c
@@ -15,3 +15,23 @@ float celsius_to_kelvin(float celsius) {
 float kelvin_to_celsius(float kelvin) {
     return kelvin - 273.15;
 }
+
+
+void categorize_temperature(float celsius) {
+    if (celsius < 0) {
+        printf("Temperature category: Freezing\n");
+        printf("Weather advisory: Wear a jacket.\n");
+    } else if (celsius < 10) {
+        printf("Temperature category: Cold\n");
+        printf("Weather advisory: Wear a jacket.\n");
+    } else if (celsius < 25) {
+        printf("Temperature category: Comfortable\n");
+        printf("Weather advisory: You should feel comfortable.\n");
+    } else if (celsius < 35) {
+        printf("Temperature category: Hot\n");
+        printf("Weather advisory: Stay hydrated.\n");
+    } else {
+        printf("Temperature category: Extreme Heat\n");
+        printf("Weather advisory: Stay indoors.\n");
+    }
+}
