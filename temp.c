35d3c8047e2b87f952fbe563edc942f4c00f775c
N-Heyer <156978640+N-Heyer@users.noreply.github.com>
2025-03-26 16:48:04 -0500

    conversions

---
+++ temp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+float celsius_to_fahrenheit(float celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+float fahrenheit_to_celsius(float fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+float celsius_to_kelvin(float celsius) {
+    return celsius + 273.15;
+}
+
+float kelvin_to_celsius(float kelvin) {
+    return kelvin - 273.15;
+}
